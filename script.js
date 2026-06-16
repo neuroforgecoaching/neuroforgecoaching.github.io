620b46e5e657811f87ac67302b417f8d2d47c1d4
Dr. Paul Giordano, PT, DPT. <paulgiordanodpt@gmail.com>
2026-06-16 17:43:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-document
-.querySelector(".contact-form")
-.addEventListener("submit", function(e){
-
-  e.preventDefault();
-
-  alert(
-    "Thank you for applying to NeuroForge. We'll contact you shortly."
-  );
-
+document.querySelector(".contact-form").addEventListener("submit", function () {
+  setTimeout(() => {
+    alert("Thank you for applying to NeuroForge. We'll contact you shortly.");
+  }, 100);
 });
